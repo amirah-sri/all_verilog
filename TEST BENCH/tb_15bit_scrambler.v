434dc309d09158fe68313fd9e7fad75a24164921
amirah-sri <67193976+amirah-sri@users.noreply.github.com>
2024-06-06 11:42:50 +0530

    Update tb_15bit_scrambler.v

--- TEST BENCH/tb_15bit_scrambler.v
+++ TEST BENCH/tb_15bit_scrambler.v
@@ -25,19 +25,16 @@ reg clk,rst;
 reg [14:0]din;
 wire [14:0]dout;
 
-scrambler i1(.clk(clk),.rst(rst),.din(din),.dout(dout));
-initial begin 
+scrambler i1(.clk(clk),.rst(rst),.initial_value(din),.dout(dout));
+initial begin //t=0
 clk=1;
 rst=1;
-#50;
-end
-
- initial begin
- rst=0;
-  for (integer i=0;i<(2**15);i=i+1) begin 
-    din = 8'b100101010000000+i;
+din = 15'b000000010101001;
+#50 rst=0;
+#50 for (integer i=0;i<((2**15)-1);i=i+1) begin 
+    din = dout;
     #10; end
-    $stop;
+     $stop;
   end
 
 always #5 clk=~clk;
